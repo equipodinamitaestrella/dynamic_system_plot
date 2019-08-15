4ac00844621bdf1599b728ac0f259d354028daa9
ktsb3 <keshava.t.s.b@hotmail.com>
2019-08-15 16:20:01 -0500

    Minor edits

--- library1.py
+++ library1.py
@@ -6,7 +6,7 @@ def generateVector(x0, x1, x2):
     aux1 = x0
     aux2 = x1
     aux3 = x2
-    for i in range 100:
+    for i in range(10):
         aux1 = aux1**2 + 1
         vect1.append(aux1)
         aux2 = aux2**2 + 1
@@ -21,4 +21,4 @@ def plot(vect):
     
     plt.title("Plotting x = x^2 + 1")
 
-    plt.plot(range(100), vect)
+    plt.plot(range(10), vect)
