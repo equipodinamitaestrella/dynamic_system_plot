43c56397007179edc1b204be15855f212a645384
stephanydvm <stephanydvm@comunidad.unam.mx>
2019-08-22 15:31:36 -0500

    Added user guide available with --help or wrong input

---
+++ help_file.py
@@ -0,0 +1,13 @@
+def helpf():
+    print("Dynamic System Plot Help")
+    print("This program plots any equation the user gives as a parameter with the following initial conditions:")
+    print("x1 = 0.1")
+    print("x2 = 1")
+    print("x3 = 10")
+    print("To run this program please execute:")
+    print("python3 main.py <flag> <paraneter>\n")
+    print("FLAGS:")
+    print("-e : Equation. This flag receives an equation as a parameter. It must be separated by a blanks pace.")
+    print("-f : File. This flag receives a file name as a parameter. It must be separated by a blank space.")
+    print("--help : Help. This flag shows a detailed guide to run 'Dynamic System Plot'.")
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from library_dynamic import *
 from library_plot import *
-
+from help_file import *
 from matplotlib import pyplot as pyplt
 
 if __name__ == "__main__":
@@ -13,9 +13,18 @@ if __name__ == "__main__":
             file_flag = True
             filename = sys.argv[i+1]
     
-        if '-e' in sys.argv[i]:
+        elif '-e' in sys.argv[i]:
             equation_flag = True
             formula_str = sys.argv[i+1]
+        
+        elif '--help' in sys.argv[i]:
+            helpf()
+            exit()
+        
+        else:
+            helpf()
+            exit()
+
     
     if file_flag:
         with open(filename) as f:
