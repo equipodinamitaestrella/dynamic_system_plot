4f2746f51caf40d335dae039a182f1b5714993ca
CoolingBroski <camarenapliego@gmail.com>
2019-08-22 14:38:03 -0500

    plotting negative values

--- library1.py
+++ library1.py
@@ -59,7 +59,7 @@ def plott(vect1, vect2, vect3, title):
     ax.plot(vect1, label = 'x = 0.1')
     ax.plot(vect2, label = 'x = 1')
     ax.plot(vect3, label = 'x = 10')
-    ax.set_yscale('log')
+    ax.set_yscale('symlog')
     ax.set(xlabel = "i", ylabel = '$x_i$', title = title)
     ax.grid()
     fig.savefig(title.replace('.',','))
