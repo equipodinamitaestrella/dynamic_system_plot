912be90a6de98b1c1a1c3fbf42c5020e2a0ec05f
CoolingBroski <camarenapliego@gmail.com>
2019-08-22 14:56:59 -0500

    main_parsing

--- library_dynamic.py
+++ library_dynamic.py
@@ -2,29 +2,7 @@ import numpy as np
 from sympy import *
 import sys
 
-def generateVector(x0, x1, x2): 
-    file_flag = False
-    equation_flag = False
-
-    n = len(sys.argv)
-    for i in range(n):
-        if '-f' in sys.argv[i]:
-            file_flag = True
-            filename = sys.argv[i+1]
-    
-        if '-e' in sys.argv[i]:
-            equation_flag = True
-            formula_str = sys.argv[i+1]
-    
-    if file_flag:
-        with open(filename) as f:
-            formula = f.readlines()
-            formula_str = formula[0]
-            print("using file mode with", formula_str)
-    
-    if equation_flag:
-        print("using equation mode with", formula_str)
-    
+def generateVector(x0, x1, x2, formula_str): 
     x = Symbol("x")
     y = sympify(formula_str)
     f = lambdify(x, y, "numpy")
--- main.py
+++ main.py
@@ -4,6 +4,28 @@ from library_plot import *
 from matplotlib import pyplot as pyplt
 
 if __name__ == "__main__":
-	vectors = generateVector(0.1,1,10)
-	plott(vectors[0], vectors[1], vectors[2], vectors[3])
-	pyplt.show()
+    file_flag = False
+    equation_flag = False
+
+    n = len(sys.argv)
+    for i in range(n):
+        if '-f' in sys.argv[i]:
+            file_flag = True
+            filename = sys.argv[i+1]
+    
+        if '-e' in sys.argv[i]:
+            equation_flag = True
+            formula_str = sys.argv[i+1]
+    
+    if file_flag:
+        with open(filename) as f:
+            formula = f.readlines()
+            formula_str = formula[0]
+            print("using file mode with", formula_str)
+    
+    if equation_flag:
+        print("using equation mode with", formula_str)
+
+    vectors = generateVector(0.1,1,10, formula_str)
+    plott(vectors[0], vectors[1], vectors[2], vectors[3])
+    pyplt.show()
