55faaf9b13cae9a9fdee2dfb06b36d5ad4634cc3
CoolingBroski <camarenapliego@gmail.com>
2019-08-23 12:00:39 -0500

    testing

--- library_plot.py
+++ library_plot.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 
 def plott(vect1, vect2, vect3, title):
     fig, ax = plt.subplots()
-    ax.plot(vect1, label = 'x = 0.1')
-    ax.plot(vect2, label = 'x = 1')
-    ax.plot(vect3, label = 'x = 10')
+    ax.plot(vect1, label = 'x = %s'%(vect1[0]))
+    ax.plot(vect2, label = 'x = %s'%(vect2[0]))
+    ax.plot(vect3, label = 'x = %s'%(vect3[0]))
     ax.set_yscale('symlog')
     ax.set(xlabel = "i", ylabel = '$x_i$', title = title)
     ax.grid()
     fig.savefig(title.replace('.',','))
+    plt.legend(loc='best')
     plt.show
--- testing.sh
+++ testing.sh
@@ -1,5 +1,4 @@
-python3 main.py -e x^2+3
-python3 main.py -e x+1
-python3 main.py -e 3*x-3
-python3 main.py -e 0.5*x
-python3 main.py -f formulae.dat
+python3 main.py -e a*x^n+b -a 1 -n 2 -b 3
+python3 main.py -e a*x^n+b -a 1 -n 1 -b 1
+python3 main.py -e a*x^n+b -a 3 -n 1 -b -3
+python3 main.py -e 0.5*x -a 0.5 -n 1 -b 0
