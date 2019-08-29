2bb8075b15fc1360061d57404e145b099d9b146a
stephanydvm <stephanydvm@comunidad.unam.mx>
2019-08-29 16:20:29 -0500

    User guide updated with range functionalities

--- help_file.py
+++ help_file.py
@@ -5,12 +5,12 @@ def helpf():
     print("python3 main.py <flag> <parameter> <flag> <parameter> ... <flag> <parameter>\n")
     print("FLAGS:")
     print("\t-e : Equation. This flag receives an equation as a parameter (e.g. ax**n + b), also the user must provide the equation's coeficients using the following flags(-a, -b, -n) and the initial conditions of the system using flag x0. Parameters must be separated by a blank space.")
-    print("\t-a : Coeficient a. This flag receives a's value. It must be separated by a blank space.")
-    print("\t-b : Coeficient b. This flag receives b's value. It must be separated by a blank space.")
-    print("\t-n : Coeficient n. This flag receives n's values. Commonly used to indicate the power of 'x'. It must be separated by a blank space.")
-    print("\t-i : Iterations. This flag receives the number of iterations the system will do. It must be at least 1 and a maximum of 7.")
-    print("\t-x0 : Initial conditions. It must be ONLY 3 initial conditions separated by a blank space.")
-    print("\t-f : File. This flag receives a file name as a parameter. File must contain input values. It must be separated by a blank space.")
+    print("\t-a : Coeficient a. This flag receives a range of a's values. Range is NOT inclusive on the right limit. It must be separated by a blank space.")
+    print("\t-b : Coeficient b. This flag receives a range of b's values. Range is NOT inclusive on the right limit. It must be separated by a blank space.")
+    print("\t-n : Coeficient n. This flag receives a range of n's values. Commonly used to indicate the power of 'x'. Range is NOT inclusive on the right limit. It must be separated by a blank space.")
+    print("\t-i : Iterations. This flag receives a number of iterations the system will do. It must be at least 1 and a maximum of 7.")
+    print("\t-x0 : Initial conditions. It must a range of initial conditions. Range is NOT inclusive on the right limit. It must be separated by a blank space.")
+    print("\t-f : File. This flag receives a file name as a parameter. File must contain input values with its proper format specified in this guide.")
     print("\t--help : Help. This flag shows a detailed user guide to run 'Dynamic System Plot'.")
 
 
