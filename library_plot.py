ab07546c4f12df9c101b086909d88e0d5dfc2862
CoolingBroski <camarenapliego@gmail.com>
2019-08-30 11:16:28 -0500

    added discrete time plotting functionality

--- library_plot.py
+++ library_plot.py
@@ -6,9 +6,11 @@ def plott(cube, title):
     for i in range(len(cube)): # iterate over matrices each representing orbits with a single equations
         for j in range(len(cube[0])): # iterate over each orbit within the given matrix
             ax.plot(cube[i][j], label = 'x = %s'%(cube[i][j][0]))
-
+    delta_time=3
+    seconds=[i*3 for i in range(len(cube[0][0]))]
     ax.set_yscale('symlog')
-    #ax.set(xlabel = "i", ylabel = '$x_i$', title = title)
+    ax.set(xlabel = "hours", ylabel = '$x_i$', title = title)
+    plt.xticks(range(len(cube[0][0])),seconds)
     ax.grid()
     fig.savefig(title.replace('.',','))
     #plt.legend(loc='best', prop={'size': 6})
