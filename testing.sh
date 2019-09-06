deacbf89cd0bd57f608830461b7c758c05979324
CoolingBroski <camarenapliego@gmail.com>
2019-09-06 11:16:48 -0500

    testing

--- testing.sh
+++ testing.sh
@@ -1,5 +1,5 @@
 python3 main.py -e a*x^n+b -a 2 3 1 -b 0 1 1 -n 1 2 1 -x0 300000 700000 100000 -i 37 -d
-python3 main.py -e a*x^n+b -a 1 2 1 -n 1 2 1 -b 3 10 1 -x0 1 30 1 -ns 5
+python3 main.py -e a*x^n+b -a 1 2 1 -n 1 2 1 -b 3 10 1 -x0 1 30 1 -ns 5 -hs 5
 python3 main.py -e a*x^n+b -a 2 4 2 -n 1 3 1 -b 1 2 0.5 -x0 1 2 0.5 -i 3 -d
 python3 main.py -e a*x^n+b -a 3 10 7 -n 2 8 4 -b -3 3 2 -x0 0.2 5 0.2 -i 3 -d
 python3 main.py -e 0.5*x -a 0.5 2 0.5 -n 1 2 1 -b 0 1 1 -x0 0.1 2 0.1
